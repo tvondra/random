dd2076ec29b362a503f361f1f6b4ed8176ca06a1
Tomas Vondra <tv@fuzzy.cz>
2024-06-13 00:15:33 +0200

    Relax test conditions to handle collisions
    The PRNG may generate some number of collisions, so do not fail the
    tests because of that.

--- test/sql/basic.sql
+++ test/sql/basic.sql
@@ -1,25 +1,25 @@
 CREATE EXTENSION random;
 
-SELECT COUNT(*), COUNT(DISTINCT random_string(1, 100, 32, 64)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_string(1, 100, 32, 64)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_bytea(1, 100, 32, 64)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_bytea(1, 100, 32, 64)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_int(1, 100, 1, 1000000)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_int(1, 100, 1, 1000000)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_bigint(1, 100, 1, 1000000)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_bigint(1, 100, 1, 1000000)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_real(1, 100, 1, 1000000)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_real(1, 100, 1, 1000000)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_double_precision(1, 100, 1, 1000000)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_double_precision(1, 100, 1, 1000000)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_numeric(1, 100, 1, 1000000)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_numeric(1, 100, 1, 1000000)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_macaddr(1, 100)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_macaddr(1, 100)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_macaddr8(1, 100)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_macaddr8(1, 100)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_inet(1, 100)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_inet(1, 100)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_cidr(1, 100)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_cidr(1, 100)) > 95 FROM generate_series(1,10000);
 
-SELECT COUNT(*), COUNT(DISTINCT random_cidr2(1, 100)) FROM generate_series(1,10000);
+SELECT COUNT(*), COUNT(DISTINCT random_cidr2(1, 100)) > 95 FROM generate_series(1,10000);
